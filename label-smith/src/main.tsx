5e4c9b1b9679a60d56df974ba1ca425ee894c50c
Ben Moore <ben@benhmoore.com>
2024-05-18 15:43:41 -0500

    Update.

--- label-smith/src/main.tsx
+++ label-smith/src/main.tsx
@@ -1,23 +1,10 @@
-<<<<<<< HEAD
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
-=======
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.scss";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
 );
->>>>>>> main
--- label-smith/vite.config.ts
+++ label-smith/vite.config.ts
@@ -1,18 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   server: {
     proxy: {
-      "/api": "http://localhost:3000",
-=======
-  css: {
-    preprocessorOptions: {
-      scss: {},
->>>>>>> main
+      '/api': 'http://localhost:3000',
     },
   },
 });
