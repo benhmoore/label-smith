1c37742860fdd4cdb96d5bc6795448a63fba1ea4
Ben Moore <ben@benhmoore.com>
2024-05-18 15:38:24 -0500

    Save state.

--- label-smith/vite.config.ts
+++ label-smith/vite.config.ts
@@ -4,9 +4,15 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+<<<<<<< HEAD
   server: {
     proxy: {
       "/api": "http://localhost:3000",
+=======
+  css: {
+    preprocessorOptions: {
+      scss: {},
+>>>>>>> main
     },
   },
 });
